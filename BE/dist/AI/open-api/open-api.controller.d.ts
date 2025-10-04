3e478b1dc82943f22349f4e9a1001260d4a92e49
Bartosz  Marcisz <bartosz@MacBook-Air-Bartosz.local>
2025-10-04 19:55:42 +0200

    cleanup

--- BE/dist/AI/open-api/open-api.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { OpenApiService } from './open-api.service';
-export declare class OpenApiController {
-    private readonly openApiService;
-    constructor(openApiService: OpenApiService);
-    getData(token: string): Promise<unknown>;
-    postData(token: string, data: any): Promise<unknown>;
-}
