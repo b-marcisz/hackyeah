26d36080d6d7c645db69ab9f2c66406958abb65a
Bartosz  Marcisz <bartosz@MacBook-Air-Bartosz.local>
2025-10-04 20:16:47 +0200

    user settings added

--- BE/src/accounts/dto/create-user.dto.ts
+++ BE/src/accounts/dto/create-user.dto.ts
@@ -1,10 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsEnum, IsOptional, IsInt } from 'class-validator';
+import { IsString, IsEnum, IsOptional, IsInt, IsObject } from 'class-validator';
 
 export enum UserRole {
   ADMIN = 'admin',
   USER = 'user',
 }
+
+export type ThemeColor =
+  | 'pinkPurple'   // #F093FB - Różowy/Fioletowy
+  | 'blue'         // #4FACFE - Niebieski
+  | 'green'        // #43E97B - Zielony
+  | 'pink'         // #FF6B9D - Różowy
+  | 'orange'       // #FFA502 - Pomarańczowy
+  | 'lavender';    // #A29BFE - Jasny fiolet
+
+export class UserSettings {
+    @ApiProperty()
+    @IsInt()
+    timeLimit: number;
+  }
 
 export class CreateUserDto {
   @ApiProperty()
@@ -15,4 +29,18 @@ export class CreateUserDto {
   @IsEnum(UserRole)
   role: UserRole;
 
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsInt()
+  age?: number;
+
+  @ApiProperty({ enum: ['pinkPurple', 'blue', 'green', 'pink', 'orange', 'lavender'], required: false })
+  @IsOptional()
+  @IsEnum(['pinkPurple', 'blue', 'green', 'pink', 'orange', 'lavender'])
+  color?: ThemeColor;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsObject()
+  settings?: UserSettings;
 }
